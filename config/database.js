c9ea998d64a61a68f2a9a7f395e82e0a8bff6e4d
Roman Lorent <Roman.Lorent@STAFFCARE.NET>
2016-05-14 13:52:35 +0100

    commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+//when deploying the app on herku please remeber to change the link var to PROD_MONGODB
+//var link = process.env.PROD_MONGODB;
+var link = "mongodb://romi103:tyfus12@ds013310.mlab.com:13310/heroku_tq5jcl24";
+
+module.exports = {
+    url: link
+};
--- server.js
+++ server.js
@@ -2,40 +2,56 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
+var Chart = require('src/chart.js')
 
-var morgan       = require('morgan');
+
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var configDB = require('./config/database.js');
+
+//static contents
+app.use(express.static(__dirname + '/public'));
 
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
-
+var db = mongoose.connection; //testing mongo db conneton
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("we're connected to database");
+});
+//
+require('./config/passport')(passport); // pass passport for configuration
+//
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
-app.set('view engine', 'ejs'); // set up ejs for templating
 
+app.set('views', './views');
+app.set('view engine', 'ejs'); // set up ejs for templating
+//
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch'
+})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+//reqiuer undefine - problem - http://sstackoverflow.com/questions/33007878/nodejs-typeerror-require-is-not-a-function
 
 // launch ======================================================================
 app.listen(port);
